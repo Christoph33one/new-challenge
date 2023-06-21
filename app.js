d9c2d3c72b01e9758b0310672b085da9bc2db4db
chris b <christopherbrowning@Christophers-MacBook-Pro.local>
2023-06-21 16:04:21 +0200

    added event listener to target id innerText and display text when click event runs

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const boxes = Array.from(document.getElementsByClassName('box'));
 console.log(boxes);
 
+const spaces = [null, null, null, null, null, null, null, null, null];
+const PLAYER_ONE = "O"; 
+const PLAYER_TWO = "X";
+let currentPlayer = PLAYER_ONE
+
 const drawBoard = () => {
     boxes.forEach((box, index) => {
         let styleString = '';
@@ -17,6 +22,20 @@ const drawBoard = () => {
             styleString += `border-top: 3px solid var(--purple);`; 
         }
         box.style = styleString;
+        box.addEventListener('click', boxClicked)
     });
 };
+
+const boxClicked = (e) => {
+    console.log('box clicked');
+    const id = e.target.id;
+    console.log(id);
+    if(!spaces[id]){
+        spaces[id] = currentPlayer;
+        e.target.innerText = currentPlayer;
+        currentPlayer = currentPlayer == PLAYER_ONE ? PLAYER_TWO : PLAYER_ONE;
+    }
+};
+
 drawBoard();
+
